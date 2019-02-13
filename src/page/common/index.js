e6ea511972988536114ac7e4f38768702b5d2615
617076674 <617076674@qq.com>
2019-02-13 13:10:57 +0800

    common

--- src/page/common/index.js
+++ src/page/common/index.js
@@ -1,3 +1,4 @@
 'use strict'
-
-console.log('i am global');
+require('./layout.css');
+require('node_modules/_font-awesome@4.7.0@font-awesome/css/font-awesome.min.css');
+require('./footer/index.css')
